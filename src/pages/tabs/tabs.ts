f9c289755adffb22c02cbee392ef10b4cbcbba1e
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-03-29 22:16:20 +0200

    changed the routes from tabs to be to the wanted pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,18 @@
+import { SettingsPage } from './../settings/settings';
+import { SearchPage } from './../search/search';
+import { ListPage } from './../list/list';
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = ListPage;
+  tab2Root = SearchPage;
+  tab3Root = SettingsPage;
 
   constructor() {
 
